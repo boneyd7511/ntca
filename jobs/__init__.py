525c1329b9e1f01a8a3390776db5b620fd94dd59
boneyd7511 <60434746+boneyd7511@users.noreply.github.com>
2024-04-02 14:00:54 -0500

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,6 +1,6 @@
 from . import *
 
-#from nautobot.apps.jobs import register_jobs
-from nautobot.core.celery import register_jobs
+from nautobot.apps.jobs import register_jobs
+#from nautobot.core.celery import register_jobs
 
 register_jobs(NewBranch)
