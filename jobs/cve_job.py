49a12de0c542ab0af6cc546fbe1d38bff044878e
boneyd7511 <60434746+boneyd7511@users.noreply.github.com>
2024-04-03 14:13:33 -0500

    test

--- jobs/cve_job.py
+++ jobs/cve_job.py
@@ -1,27 +1,36 @@
 from django.contrib.contenttypes.models import ContentType
 
-#from nautobot.apps.jobs import Job, register_jobs
 from nautobot.core.celery import register_jobs
 from nautobot.apps.jobs import Job, StringVar, IntegerVar, ObjectVar
-from nautobot.dcim.models import Location, LocationType, Device, Manufacturer, DeviceType
+from nautobot.dcim.models.devices import Location, LocationType, Device, Manufacturer, DeviceType, Platform
 from nautobot.extras.models import Status, Role
-from nautobot_device_lifecycle_mgmt.models import CVELCM
+from nautobot_device_lifecycle_mgmt.models import CVELCM, SoftwareLCM
+
+import requests
+from bs4 import BeautifulSoup
 
 
 class ProvisionCVE(Job):
     class Meta:
         name = "Provision CVEs"
         description = "Pull CVEs from the internet and propogate them into Nautobot."
-
+        
     def run(self):
-        # Create the new location
-
+        
+        #LocationType.objects.get_or_create(name="Campus")
+        
         cve_name = "CVE-2024-20303"
         cve_published_date = "2024-04-02"
         cve_link = "https://sec.cloudapps.cisco.com/security/center/content/CiscoSecurityAdvisory/cisco-sa-wlc-mdns-dos-4hv6pBGf"
+        
+        cve_name2 = "CVE-2024-203034"
+        cve_published_date2 = "2024-04-02"
+        cve_link2 = "https://sec.cloudapps.cisco.com/security/center/content/CiscoSecurityAdvisory/cisco-sa-wlc-mdns-dos-4hv6pBGf2"
 
         cve = CVELCM(name=cve_name, published_date=cve_published_date, link=cve_link)
+        cve2 = CVELCM(name=cve_name2, published_date=cve_published_date2, link=cve_link2)
 
         cve.validated_save()
+        cve2.validated_save()
 
 register_jobs(ProvisionCVE)
